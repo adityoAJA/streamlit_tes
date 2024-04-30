e25dff41805b79cd6698ab4562b94a52416b84b6
adityoAJA <168532434+adityoAJA@users.noreply.github.com>
2024-04-30 17:39:48 +0700

    Update 4_Dashboard.py

--- pages/4_Dashboard.py
+++ pages/4_Dashboard.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 st.markdown("# Oceanography ❄️")
 st.sidebar.markdown("# Page 3 ❄️")
-nc_file = "data\indo_pr_day_EC-Earth3_historical_r1i1p1f1_gr_2013.nc"
+nc_file = "indo_pr_day_EC-Earth3_historical_r1i1p1f1_gr_2013.nc"
 data = xr.open_dataset(nc_file)
 temperature = data['pr'].isel(time=1).values # Selecting a single time slice for plotting
 lats = data['lat'].values
